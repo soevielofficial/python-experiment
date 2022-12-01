84dc3fdb068ab57d0f11555563dfa3c2cc3de67c
soevielofficial <soevielofficial@gmail.com>
2022-12-01 22:08:53 +0700

    Create computing.py

---
+++ computing.py
@@ -0,0 +1,11 @@
+import time
+def compute(data):
+    start = time.time()
+    
+    # Enter the function or code to be calculated
+
+    finish = time.time()
+
+    total = finish-start
+
+    print(f"Total computation time: {total} second")

9f48ed7951b6202c547ef2727196b16c310c3552
soevielofficial <soevielofficial@gmail.com>
2022-12-03 21:28:34 +0700

    modified:   crud.py

--- crud.py
+++ crud.py
@@ -1,4 +1,6 @@
-mahasiswa = []
+# a simple CRUD (Create, Read, Update, Delete) program for processing student name data.
+
+student = []
 
 def menu():
     print("------- MENU -------")
@@ -11,58 +13,58 @@ def menu():
 active = False
 def main():
     active = True
-    menu()
     while active:
+        menu()
         print("Select a menu by entering the menu number.")
         selected = int(input())
         if(selected == 1):
-            show_data_mahasiswa()
+            show_data_student()
         elif(selected == 2):
-            insert_data_mahasiswa()
+            insert_data_student()
         elif(selected == 3):
-            edit_data_mahasiswa()
+            edit_data_student()
         elif(selected == 4):
-            delete_data_mahasiswa()
+            delete_data_student()
         elif(selected == 5):
             active = False
         else:
             print("Enter the correct data!!!")
         print()
 
-def show_data_mahasiswa():
+def show_data_student():
     no = 1
     print("Student List: ")
-    for item in mahasiswa:
+    for item in student:
         print(f'{no}. {item}')
         no +=1
 
-def insert_data_mahasiswa():
-    mahasiswa_len = len(mahasiswa)
-    mahasiswa_input = input("Enter student name: ")
-    mahasiswa.insert(mahasiswa_len, mahasiswa_input)
-    show_data_mahasiswa()
+def insert_data_student():
+    student_len = len(student)
+    student_input = input("Enter student name: ")
+    student.insert(student_len, student_input)
+    show_data_student()
 
-def edit_data_mahasiswa():
+def edit_data_student():
     no = 1
     print("Student List: ")
-    for item in mahasiswa:
+    for item in student:
         print(f'[{no}]. {item}')
         no +=1
 
     edit_selected = int(input("Select the student serial number, to edit the student name."))
-    mahasiswa[edit_selected-1] = input(f"Edit name'{mahasiswa[edit_selected-1]}' become: ")
-    show_data_mahasiswa()
+    student[edit_selected-1] = input(f"Edit name'{student[edit_selected-1]}' become: ")
+    show_data_student()
 
-def delete_data_mahasiswa():
+def delete_data_student():
     print("Student List: ")
     no = 1
-    for item in mahasiswa:
+    for item in student:
         print(f'[{no}]. {item}')
         no +=1
 
     del_selected = int(input("Select the serial number of students, to delete student names: "))
-    mahasiswa.pop(del_selected-1)
+    student.pop(del_selected-1)
     print()
-    show_data_mahasiswa
+    show_data_student
 
 main()
